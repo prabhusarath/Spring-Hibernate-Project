ce24f63b9d9ecd83d07379b3d5193b39903ff2b4
prabhusarath <prabhu.sarath@gmail.com>
2017-06-27 12:03:59 -0500

    Java Persistence

---
+++ jpa/src/main/java/io/sarath/App.java
@@ -0,0 +1,13 @@
+package io.sarath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
